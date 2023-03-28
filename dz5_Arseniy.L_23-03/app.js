85d51b3397dcbef762e3c3f3c70fc75d837949fc
arseniy2006 <59921629+arseniy2006@users.noreply.github.com>
2023-03-28 20:57:15 +0600

    Update app.js

--- dz5_Arseniy.L_23-03/app.js
+++ dz5_Arseniy.L_23-03/app.js
@@ -1,28 +1,3 @@
-// const som = document.querySelector('#som')
-// const usd = document.querySelector('#usd')
-// const convert = (elem, target, isTrue) => {
-//     elem.oninput= () => {
-//         const request = new XMLHttpRequest()
-//         request.open("GET", "data.json")
-//         request.setRequestHeader("Content-type", "application/json")
-//         request.send()
-//         request.onload = () => {
-//             const response = JSON.parse(request.responseText)
-//             if (isTrue) {
-//                 target.value = (elem.value / response.usd).toFixed(2)
-//             } else {
-//                 target.value = (elem.value * response.usd).toFixed(2)
-//             }
-//             elem.value === '' && (target.value = '')
-//         }
-//     }
-// }
-//
-//
-// convert(som, usd, true)
-// convert(usd, som, false)
-
-
 const som = document.querySelector('#som')
 const usd = document.querySelector('#usd')
 const eur = document.querySelector('#eur')
